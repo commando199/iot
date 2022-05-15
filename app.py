4ec91874198f967bea79ddb736e3165958dfb724
commando199 <roccoalesch.manz@gmail.com>
2022-05-15 15:45:05 +0200

    changed accept

--- app.py
+++ app.py
@@ -64,31 +64,31 @@ def news_display():
 
 @app.route("/accepted")
 def accepted():
-    url1 = 'https://siddhi4.bpmcep.ics.unisg.ch/engine-rest/task/'
-    response = requests.post(url1,json={})
+    try:
+        first_time = request.args.get('time')
+    except:
+        first_time = 0
+    if first_time:
+        url1 = 'https://siddhi4.bpmcep.ics.unisg.ch/engine-rest/task/'
+        response = requests.post(url1,json={})
 
-    print(response)
-    responseJson = response.json()
+        print(response)
+        responseJson = response.json()
 
-    taskId = responseJson[0]['id']  # get ID of the task
-    url = f'https://siddhi4.bpmcep.ics.unisg.ch/engine-rest/task/{taskId}/complete'
-    new_request = {
-        "variables": {"approved": {"value": True},
-                      "item": {"value": "stocks"},
-                      "quantity": {"value": 2},
-                      "repeats": {"value": 0}
-                      }
-    }
-    complete_url = url
+        taskId = responseJson[0]['id']  # get ID of the task
+        url = f'https://siddhi4.bpmcep.ics.unisg.ch/engine-rest/task/{taskId}/complete'
+        new_request = {
+            "variables": {"approved": {"value": True},
+                          "item": {"value": "stocks"},
+                          "quantity": {"value": 2},
+                          "repeats": {"value": 0}
+                          }
+        }
+        complete_url = url
 
-    # Call method "complete" with prepared url
-    complete = requests.post(complete_url, json=new_request)
-    print('complete status code: ', complete.status_code)
-
-
-
-
-
+        # Call method "complete" with prepared url
+        complete = requests.post(complete_url, json=new_request)
+        print('complete status code: ', complete.status_code)
 
     return render_template('accepted.html')
 
